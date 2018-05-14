ff40fa35592436b0606756586412ae9341b05123
Connor Onweller <onweller@udel.edu>
2018-05-14 15:01:04 -0400

    added var and norm

--- Project.R
+++ Project.R
@@ -8,3 +8,23 @@ n2<-length(y1)
 CSK<-as.character(x[1,3])
 DI<-as.character(x[2,3])
 x1y1<-c(x1,y1)
+
+
+isDependent<-function(){DI=='D'}
+
+isNormal<-function(vec){
+  pVal<-shapiro.test(vec)$p.value
+  return(pVal > 0.05)
+}
+
+isEqualVar<-function(){
+  sdX <- sd(x1)
+  sdY <- sd(y1)
+  if (sdX > sdY){
+    return ((sdX/sdY) < 2)
+  }
+  else{
+    return ((sdY/sdX) < 2)
+  }
+}
+
